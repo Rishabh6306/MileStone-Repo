40a89a4b6bb8f2ef47b33f478089a0faa6eb9c7b
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-07-29 17:25:08 +0530

    Projecte-1 Blog Website Created Succefully

--- Project-3/src/Components/remove.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function remove(props) {
-    return (
-        <div>
-            localStorage.removeItem({props.removeItems});
-        </div>
-    )
-}
